b79359b8e734d2a0f32a4c0cd369b478ddb1a916
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2023-01-20 22:38:33 +0530

    The BETWEEN Operator

---
+++ 5 The BETWEEN Operator.sql
@@ -0,0 +1,8 @@
+SELECT * FROM customers
+WHERE points >=1000 AND points <=3000;
+
+SELECT * FROM customers
+WHERE points BETWEEN 1000 AND 3000;
+
+SELECT * FROM customers
+WHERE birth_date BETWEEN '1990-01-01' AND '2000-01-01';
