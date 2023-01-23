217d4970f5cdff44dcaa96b47bb34012ae403e03
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2023-01-23 16:51:50 +0530

    Outer Joins

---
+++ 17 Outer Joins.sql
@@ -0,0 +1,16 @@
+SELECT c.customer_id, c.first_name, o.order_id
+FROM customers c
+LEFT JOIN orders o
+ON c.customer_id = o.customer_id;
+
+SELECT c.customer_id, c.first_name, o.order_id
+FROM customers c
+RIGHT JOIN orders o
+ON c.customer_id = o.customer_id;
+
+-- Exercise
+
+SELECT p.product_id, p.name , oi.quantity
+FROM products p
+LEFT JOIN order_items oi
+ON p.product_id = oi.product_id;
