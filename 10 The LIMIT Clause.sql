44dafd63a2b22c1953d75fe025827b0e352c3f2d
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2023-01-23 12:07:33 +0530

    The LIMIT Clause

---
+++ 10 The LIMIT Clause.sql
@@ -0,0 +1,13 @@
+SELECT * FROM customers
+LIMIT 3;
+
+SELECT * FROM customers
+LIMIT 300;
+
+SELECT * FROM customers
+LIMIT 6,3;
+
+-- Exercise
+SELECT * FROM customers
+ORDER BY points DESC 
+LIMIT 3;
--- 9 The ORDER BY Clause.sql
+++ 9 The ORDER BY Clause.sql
@@ -24,5 +24,3 @@ SELECT order_id, product_id, quantity, unit_price
 FROM order_items
 WHERE order_id =2  
 ORDER BY  (quantity * unit_price) DESC;
-
-
