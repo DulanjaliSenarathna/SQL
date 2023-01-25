fa119a73be7c531cc486f6407a357643d49960af
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2023-01-25 19:12:48 +0530

    Inserting Multiple Rows

---
+++ 25 Inserting Multiple Rows.sql
@@ -0,0 +1,7 @@
+use sql_store;
+INSERT INTO shippers (name)
+VALUES ('Shipper1'),('Shipper2'),('Shipper3');
+
+-- exercise
+INSERT INTO products
+VALUES (DEFAULT,'Book',5,1.00),(DEFAULT,'Pen',10,3.50),(DEFAULT,'Pencil',5,1.00);
