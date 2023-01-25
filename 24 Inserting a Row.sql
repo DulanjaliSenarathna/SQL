bc1111b02832782cdb090d92955772ea24d693c4
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2023-01-25 16:59:28 +0530

    Inserting a Row

---
+++ 24 Inserting a Row.sql
@@ -0,0 +1,7 @@
+INSERT INTO customers
+VALUES
+(DEFAULT ,'Dulsjsli','Senarathna','1993-09-29',NULL,'address','city','CA',DEFAULT);
+use sql_store;
+INSERT INTO customers (first_name,last_name, birth_date,address,city,state)
+VALUES
+('Dulsjsli','Senarathna','1993-09-29','address','city','CA');
