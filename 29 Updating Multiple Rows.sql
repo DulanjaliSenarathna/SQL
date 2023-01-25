b0b853cedb5fc57bf573c94397bec32f1725d88b
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2023-01-25 22:22:05 +0530

    Updating Multiple Rows

---
+++ 29 Updating Multiple Rows.sql
@@ -0,0 +1,15 @@
+use sql_invoicing;
+UPDATE invoices
+SET payment_total = invoice_total * 0.5, payment_date = due_date
+WHERE client_id = 3;
+
+use sql_invoicing;
+UPDATE invoices
+SET payment_total = invoice_total * 0.5, payment_date = due_date
+WHERE client_id IN (3,4);
+
+-- exercise
+use sql_store;
+UPDATE customers
+SET points = points + 50
+WHERE birth_date < '1990-01-01';
