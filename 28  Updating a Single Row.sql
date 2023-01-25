1fe70b40a04b3e956e9401fe4708725a5f8562c6
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2023-01-25 21:48:23 +0530

    Updating a Single Row

---
+++ 28  Updating a Single Row.sql
@@ -0,0 +1,7 @@
+UPDATE invoices
+SET payment_total = 10, payment_date = '2019-03-01'
+WHERE invoice_id = 1;
+
+UPDATE invoices
+SET payment_total = invoice_total * 0.5, payment_date = due_date
+WHERE invoice_id = 3;
