47ade932d9660f8299d0b74bb476428e9cb6bfcf
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2023-01-27 14:06:04 +0530

    Numeric Functions

---
+++ 7. Essential MySQL Functions/1 Numeric Functions.sql
@@ -0,0 +1,6 @@
+SELECT ROUND(5.73,1);
+SELECT TRUNCATE(5.733,1); -- keep 1 digit after decimal point and remove others
+SELECT CEILING(5.73); -- return the smallest int that is greater than or equal to number in paranthesies
+SELECT FLOOR(5.73);-- return the largest int that is less than or equal to number in paranthesies
+SELECT ABS(-5.73); -- return the absolute value
+SELECT RAND(); -- retun random number between 1 and 0
