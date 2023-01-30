5696f2015cb6166690b5149735d7204ebd7713ca
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2023-01-30 13:09:03 +0530

    Altering or Dropping Views

---
+++ 8. Views/2 Altering or Dropping Views.sql
@@ -0,0 +1,21 @@
+DROP VIEW sales_by_client;
+
+CREATE VIEW sales_by_client AS
+SELECT 
+c.client_id,
+c.name,
+SUM(invoice_total) AS total_sales
+FROM clients c 
+JOIN invoices i
+USING (client_id)
+GROUP BY client_id, name;
+
+CREATE OR REPLACE VIEW sales_by_client AS
+SELECT 
+c.client_id,
+c.name,
+SUM(invoice_total) AS total_sales
+FROM clients c 
+JOIN invoices i
+USING (client_id)
+GROUP BY client_id, name;
