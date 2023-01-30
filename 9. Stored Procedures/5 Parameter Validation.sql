7aec6647e94749d6033a11b4df785c0ff942d1e6
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2023-01-30 19:57:14 +0530

    Parameter Validation

---
+++ 9. Stored Procedures/5 Parameter Validation.sql
@@ -0,0 +1,13 @@
+CREATE DEFINER=`root`@`localhost` PROCEDURE `make_payment`(invoice_id INT, payment_amount DECIMAL(9,2), payment_date DATE)
+BEGIN
+	IF payment_amount <= 0 THEN 
+		SIGNAL SQLSTATE '22003'
+			SET MESSAGE_TEXT = 'Invalid payment amount';
+	END IF;
+	UPDATE invoices i
+    SET 
+		i.payment_total = payment_amount,
+        i.payment_date = payment_date
+	WHERE
+		i.invoice_id  = invoice_id;
+END
