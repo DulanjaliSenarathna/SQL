eda3554890e7d49a08e5b84778c460d434347020
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2023-02-02 16:43:02 +0530

    Order of Columns in Composite Indexes

---
+++ 14 Indexing/6 Order of Columns in Composite Indexes.sql
@@ -0,0 +1,17 @@
+EXPLAIN SELECT customer_id
+FROM customers
+WHERE state = 'CA' AND last_name LIKE 'A%';
+
+CREATE INDEX idx_state_lastname ON customers (state, last_name); 
+CREATE INDEX idx_lastname_state ON customers (last_name,state); 
+
+EXPLAIN SELECT customer_id
+FROM customers
+USE INDEX (idx_lastname_state)
+WHERE LIKE 'A%' AND last_name LIKE 'A%';
+
+EXPLAIN SELECT customer_id
+FROM customers
+USE INDEX (idx_state_lastname)
+WHERE state LIKE 'A%' AND last_name LIKE 'A%';
+
